c0416c13911dc00d0be995467fc35427eb1a3206
Renaud LEGAN <rlegan@student.42.fr>
2020-10-16 21:29:44 +0200

    added goals command

--- src/main.py
+++ src/main.py
@@ -6,14 +6,26 @@ from constants import TOKEN
 bot = commands.Bot(command_prefix='z!')
 
 
-def create_link(display, name):
-    return '[' + display + ']' + '(https://www.twitch.tv/' + name + ')'
-
-
 async def get_data():
     response = requests.get('https://zevent.fr/api/data.json')
     json = response.json()
     return json
+
+
+def create_link(display, name):
+    return '[' + display + ']' + '(https://www.twitch.tv/' + name + ')'
+
+
+def create_goal(goal, donation_amout):
+    icon = '✅' if donation_amout > goal['amountRequired']['number'] else '❌'
+    return goal['amountRequired']['formatted'] + ' -> ' + icon + ' ' + goal['title']
+
+
+def check_streamer_name(data, streamer):
+    for element in data['live']:
+        if (element['display'].lower() == streamer.lower() or element['twitch'].lower() == streamer.lower()):
+            return element
+    return None
 
 
 @bot.event
@@ -21,6 +33,36 @@ async def on_ready():
     print('the bot is up and running !')
     print('username : {}'.format(bot.user.name))
     print('user id : {}'.format(bot.user.id))
+
+
+@bot.command()
+async def goal(ctx, arg):
+    data = await get_data()
+    streamer = check_streamer_name(data, arg)
+    goal = ''
+
+    if (streamer != None):
+        totalDonationNumber = streamer['donationGoal']['donationAmount']['number']
+        totalDonationFormatted = streamer['donationGoal']['donationAmount']['formatted']
+
+        goal += '\nCURRENTLY AT ***' + totalDonationFormatted + \
+            '***  ON ' + streamer['display'] + " STREAM\n\n"
+
+        for element in streamer['donationGoal']['goals']:
+            goal += create_goal(element, int(totalDonationNumber)) + '\n'
+
+        embed = discord.Embed(title='DONATIONS GOALS FOR : ' + streamer['display'],
+                              description=goal, color=0x4bba30)
+        await bot.get_channel(ctx.message.channel.id).send(
+            embed=embed)
+
+    elif (streamer != None and len(streamer['donationGoal']) == 0):
+        await bot.get_channel(ctx.message.channel.id).send(
+            streamer['display'] + 'has no donation goals setupped on the ZEVENT site.')
+
+    else:
+        await bot.get_channel(ctx.message.channel.id).send(
+            'Please provide a streamer that is present to the ZEVENT 2020 or check the name you provided.')
 
 
 @bot.command()
